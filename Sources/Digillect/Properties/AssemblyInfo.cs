82a7aa55224c55b362e30d490f45d0ab3aa6efa0
Andrew Nefedkin <Andrew.Nefedkin@actis.ru>
2012-03-12 15:25:59 +0400

    SemVer.

--- Sources/Digillect/Properties/AssemblyInfo.cs
+++ Sources/Digillect/Properties/AssemblyInfo.cs
@@ -3,16 +3,16 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyTitle("Digillect")]
+[assembly: AssemblyDescription("Digillect Common Libraries: Core")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
 [assembly: AssemblyConfiguration("Retail")]
 #endif
-[assembly: AssemblyTitle( "Digillect" )]
-[assembly: AssemblyDescription( "Digillect Common Libraries: Core" )]
 [assembly: AssemblyCompany( "Actis Systems" )]
 [assembly: AssemblyProduct("Digillect® Common Libraries")]
-[assembly: AssemblyCopyright("© 2002-2011 Actis Systems. All rights reserved.")]
+[assembly: AssemblyCopyright("© 2002-2012 Actis Systems. All rights reserved.")]
 [assembly: AssemblyTrademark("Digillect is a registered trademark of Actis Systems.")]
 
 [assembly: AssemblyVersion(AssemblyInfo.Version)]
@@ -28,11 +28,11 @@ internal static class AssemblyInfo
 {
 	public const string Major = "4";
 	public const string Minor = "0";
-	public const string Build = "0";
-	public const string Revision = "0";
+	public const string Patch = "0";
+	public const string SemVerSuffix = "";
 
-	public const string Version = Major + "." + Minor + "." + Build + ".0";
-	public const string FileVersion = Major + "." + Minor + "." + Build + "." + Revision;
+	public const string Version = Major + "." + Minor + "." + Patch;
+	public const string FileVersion = Major + "." + Minor + "." + Patch + SemVerSuffix;
 	public const string ProductVersion = Major + "." + Minor;
-	public const string SatelliteContractVersion = Major + "." + Minor + ".0.0";
+	public const string SatelliteContractVersion = Major + "." + Minor;
 }
