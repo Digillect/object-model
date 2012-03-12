90d68084f239a3e58c66eecbf4df7bd6d52b7119
Andrew Nefedkin <Andrew.Nefedkin@actis.ru>
2012-03-12 13:43:43 +0400

    "Приведение в чувство".

--- Sources/Digillect/GlobalSuppressions.cs
+++ Sources/Digillect/GlobalSuppressions.cs
@@ -1,3 +1,13 @@
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the 
+// Error List, point to "Suppress Message(s)", and click 
+// "In Project Suppression File".
+// You do not need to add suppressions to this file manually.
+
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1019:DefineAccessorsForAttributeArguments", Scope = "type", Target = "Digillect.Runtime.CompilerServices.AutoNotifyAdditionalPropertiesAttribute")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1019:DefineAccessorsForAttributeArguments", Scope = "type", Target = "Digillect.Runtime.CompilerServices.AutoNotifyPropertyChangesAttribute")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1019:DefineAccessorsForAttributeArguments", Scope = "type", Target = "Digillect.Runtime.CompilerServices.XKeyAttribute")]
