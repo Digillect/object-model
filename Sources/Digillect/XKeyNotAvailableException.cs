e0da89745167bbabc8e4be8070042cd64c18c22a
Andrew Nefedkin <Andrew.Nefedkin@actis.ru>
2012-04-02 21:58:25 +0400

    Resurrected XKeyNotAvailableException.

---
+++ Sources/Digillect/XKeyNotAvailableException.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Digillect
+{
+#if !SILVERLIGHT
+	/// <summary>
+	/// Exception that is thrown when <see cref="Digillect.XObject"/> can not provide valid key.
+	/// </summary>
+	[Serializable]
+#endif
+	public sealed class XKeyNotAvailableException : Exception
+	{
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the <see cref="XKeyNotAvailableException"/> class.
+		/// </summary>
+		public XKeyNotAvailableException()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="XKeyNotAvailableException"/> class.
+		/// </summary>
+		/// <param name="message">The message.</param>
+		public XKeyNotAvailableException( string message )
+			: base(message)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="XKeyNotAvailableException"/> class.
+		/// </summary>
+		/// <param name="message">The message.</param>
+		/// <param name="innerException">The inner exception.</param>
+		public XKeyNotAvailableException( string message, Exception innerException )
+			: base(message, innerException)
+		{
+		}
+
+#if !SILVERLIGHT
+		private XKeyNotAvailableException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+#endif
+		#endregion
+	}
+}
