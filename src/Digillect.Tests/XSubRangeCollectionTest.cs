dcb421be906e495b0bb5ef7e3c6d18b8da647995
Andrew Nefedkin <andrew.nefedkin@actis.ru>
2012-11-27 19:46:23 +0400

    Refactored tests.

--- src/Digillect.Tests/XSubRangeCollectionTest.cs
+++ src/Digillect.Tests/XSubRangeCollectionTest.cs
@@ -11,83 +11,265 @@ namespace Digillect.Tests
 {
 	public class XSubRangeCollectionTest
 	{
+		private const int Many = 3;
+		private const int TooMany = 6;
+
 		[Fact]
-		public void CountTest()
+		public void CountWillReturnZeroWithTotalElementsBelowStartIndex()
 		{
-			var c = CreateCollection(5);
+			// Setup
+			const int expectedResult = 0;
+			var sut = new XSubRangeCollection<XObject>(CreateCollection(Many), Many, 1);
 
-			// Too few elements - count should be zero
-			Assert.Equal(0, new XSubRangeCollection<XObject>(c, c.Count, 1).Count);
+			// Exercise
+			int result = sut.Count;
 
-			// Not enough elements
-			for ( int i = 0; i <= c.Count; i++ )
-			{
-				Assert.Equal(c.Count - i, new XSubRangeCollection<XObject>(c, i, c.Count + 1).Count);
-			}
-
-			// Enough elements
-			for ( int i = 0; i <= c.Count; i++ )
-			{
-				Assert.Equal(c.Count - i, new XSubRangeCollection<XObject>(c, i, c.Count - i).Count);
-			}
-
-			for ( int i = 0; i < c.Count; i++ )
-			{
-				Assert.Equal(c.Count - i - 1, new XSubRangeCollection<XObject>(c, i + 1, c.Count - i - 1).Count);
-			}
+			// Verify
+			Assert.Equal<int>(expectedResult, result);
 		}
 
 		[Fact]
-		public void IndexerTest()
+		public void CountWillReturnNonZeroBelowMaxCountWithNotEnoughElements()
 		{
-			var c = CreateCollection(5);
-			var cr = new XSubRangeCollection<XObject>(c, 1, c.Count - 2);
+			// Setup
+			const int notEnough = TooMany;
+			const int startIndex = TooMany - Many;
+			const int maxCount = TooMany;
+			const int expectedResult = notEnough - startIndex;
+			var sut = new XSubRangeCollection<XObject>(CreateCollection(TooMany), startIndex, maxCount);
 
-			Assert.Equal(2, c.IndexOf(cr[1]));
+			// Exercise
+			int result = sut.Count;
+
+			// Verify
+			Assert.InRange(result, 1, maxCount);
+			Assert.Equal<int>(expectedResult, result);
 		}
 
 		[Fact]
-		public void ContainsItemTest()
+		public void CountWillReturnMaxCountWithEnoughElements()
 		{
-			var c = CreateCollection(5);
-			var cr = new XSubRangeCollection<XObject>(c, 1, c.Count - 2);
+			// Setup
+			const int expectedResult = Many;
+			var c = CreateCollection(TooMany);
 
-			Assert.False(cr.Contains(c[0]));
-			Assert.True(cr.Contains(c[2]));
-			Assert.False(cr.Contains(c[c.Count - 1]));
+			// Verify at left bound
+			var sut = new XSubRangeCollection<XObject>(c, 0, Many);
+			int result = sut.Count;
+			Assert.Equal<int>(expectedResult, result);
+
+			// Verify inside bounds
+			sut = new XSubRangeCollection<XObject>(c, TooMany - Many - 1, Many);
+			result = sut.Count;
+			Assert.Equal<int>(expectedResult, result);
+
+			// Verify at right bound
+			sut = new XSubRangeCollection<XObject>(c, TooMany - Many, Many);
+			result = sut.Count;
+			Assert.Equal<int>(expectedResult, result);
 		}
 
 		[Fact]
-		public void ContainsKeyTest()
+		public void ItemIndexerWillReturnCorrectlyAdjustedValue()
 		{
-			var c = CreateCollection(5);
-			var cr = new XSubRangeCollection<XObject>(c, 1, c.Count - 2);
+			// Setup
+			const int startIndex = 1;
+			const int testIndex = 1;
+			var c = CreateCollection(TooMany);
+			XObject expectedResult = c[startIndex + testIndex];
+			var sut = new XSubRangeCollection<XObject>(c, startIndex, Many);
 
-			Assert.False(cr.ContainsKey(c[0].GetKey()));
-			Assert.True(cr.ContainsKey(c[2].GetKey()));
-			Assert.False(cr.ContainsKey(c[c.Count - 1].GetKey()));
+			// Excercise
+			XObject result = sut[testIndex];
+
+			// Verify
+			Assert.Same(expectedResult, result);
 		}
 
 		[Fact]
-		public void IndexOfItemTest()
+		public void ContainsItemWillReturnTrueForInnerElements()
 		{
-			var c = CreateCollection(5);
-			var cr = new XSubRangeCollection<XObject>(c, 1, c.Count - 2);
+			// Setup
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 1, 2);
+			XObject obj = c[2];
 
-			Assert.Equal(-1, cr.IndexOf(c[0]));
-			Assert.Equal(1, cr.IndexOf(c[2]));
-			Assert.Equal(-1, cr.IndexOf(c[c.Count - 1]));
+			// Excercise
+			bool result = sut.Contains(obj);
+
+			// Verify
+			Assert.True(result);
 		}
 
 		[Fact]
-		public void IndexOfKeyTest()
+		public void ContainsItemWillReturnFalseForBelowOuterElements()
 		{
-			var c = CreateCollection(5);
-			var cr = new XSubRangeCollection<XObject>(c, 1, c.Count - 2);
+			// Setup
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 1, 2);
+			XObject obj = c[0];
 
-			Assert.Equal(-1, cr.IndexOf(c[0].GetKey()));
-			Assert.Equal(1, cr.IndexOf(c[2].GetKey()));
-			Assert.Equal(-1, cr.IndexOf(c[c.Count - 1].GetKey()));
+			// Excercise
+			bool result = sut.Contains(obj);
+
+			// Verify
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ContainsItemWillReturnFalseForBeyondOuterElements()
+		{
+			// Setup
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 0, TooMany - 1);
+			XObject obj = c[sut.Count];
+
+			// Excercise
+			bool result = sut.Contains(obj);
+
+			// Verify
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ContainsKeyWillReturnTrueForInnerElements()
+		{
+			// Setup
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 1, 2);
+			XKey key = c[2].GetKey();
+
+			// Excercise
+			bool result = sut.ContainsKey(key);
+
+			// Verify
+			Assert.True(result);
+		}
+
+		[Fact]
+		public void ContainsKeyWillReturnFalseForBelowOuterElements()
+		{
+			// Setup
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 1, 2);
+			XKey key = c[0].GetKey();
+
+			// Excercise
+			bool result = sut.ContainsKey(key);
+
+			// Verify
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ContainsKeyWillReturnFalseForBeyondOuterElements()
+		{
+			// Setup
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 0, TooMany - 1);
+			XKey key = c[sut.Count].GetKey();
+
+			// Excercise
+			bool result = sut.ContainsKey(key);
+
+			// Verify
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void IndexOfItemWillReturnCorrectValueForInnerElements()
+		{
+			// Setup
+			const int expectedResult = 1;
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 1, 2);
+			XObject obj = c[2];
+
+			// Excercise
+			int result = sut.IndexOf(obj);
+
+			// Verify
+			Assert.Equal<int>(expectedResult, result);
+		}
+
+		[Fact]
+		public void IndexOfItemWillReturnMinusOneForBelowOuterElements()
+		{
+			// Setup
+			const int expectedResult = -1;
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 1, 2);
+			XObject obj = c[0];
+
+			// Excercise
+			int result = sut.IndexOf(obj);
+
+			// Verify
+			Assert.Equal<int>(expectedResult, result);
+		}
+
+		[Fact]
+		public void IndexOfItemWillReturnMinusOneForBeyondOuterElements()
+		{
+			// Setup
+			const int expectedResult = -1;
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 0, TooMany - 1);
+			XObject obj = c[sut.Count];
+
+			// Excercise
+			int result = sut.IndexOf(obj);
+
+			// Verify
+			Assert.Equal<int>(expectedResult, result);
+		}
+
+		[Fact]
+		public void IndexOfKeyWillReturnCorrectValueForInnerElementsest()
+		{
+			// Setup
+			const int expectedResult = 1;
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 1, 2);
+			XKey key = c[2].GetKey();
+
+			// Excercise
+			int result = sut.IndexOf(key);
+
+			// Verify
+			Assert.Equal<int>(expectedResult, result);
+		}
+
+		[Fact]
+		public void IndexOfKeyWillReturnMinusOneForBelowOuterElements()
+		{
+			// Setup
+			const int expectedResult = -1;
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 1, 2);
+			XKey key = c[0].GetKey();
+
+			// Excercise
+			int result = sut.IndexOf(key);
+
+			// Verify
+			Assert.Equal<int>(expectedResult, result);
+		}
+
+		[Fact]
+		public void IndexOfKeyWillReturnMinusOneForBeyondOuterElements()
+		{
+			// Setup
+			const int expectedResult = -1;
+			var c = CreateCollection(TooMany);
+			var sut = new XSubRangeCollection<XObject>(c, 0, TooMany - 1);
+			XKey key = c[sut.Count].GetKey();
+
+			// Excercise
+			int result = sut.IndexOf(key);
+
+			// Verify
+			Assert.Equal<int>(expectedResult, result);
 		}
 
 		private static IXList<XObject> CreateCollection(int count)
