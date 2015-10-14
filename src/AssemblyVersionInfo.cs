3ef899aba080bc114719ab43e38c8ea9c485b6c7
Andrew Nefedkin <andrew.nefedkin@actis.ru>
2015-10-14 17:34:34 +0300

    Introduced per-project packages.config files.
    Added xUnit.net runner via its NuGet package.
    Upgraded all NuGet packages.

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -1,15 +1,15 @@
 ﻿#region Copyright (c) 2002-2014 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman)
 // Copyright (c) 2002-2014 Gregory Nickonov and Andrew Nefedkin (Actis® Wunderman).
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
 // without restriction, including without limitation the rights to use, copy, modify, merge,
 // publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
 // to whom the Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or
 // substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -31,7 +31,7 @@ internal static class AssemblyInfo
 {
 	public const string Major = "4";
 	public const string Minor = "1";
-	public const string Revision = "2";
+	public const string Revision = "3";
 	public const string BuildNumber = "0";
 
 	public const string Version = Major + "." + Minor + "." + Revision + ".0";
