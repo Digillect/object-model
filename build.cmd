84d2bf0fef19795cb5bab1d6070bbf433e3d0aa9
Andrew Nefedkin <andrew.nefedkin@actis.ru>
2016-07-15 13:38:48 +0300

    Updated build tools to version 2.0.0-rc.

--- build.cmd
+++ build.cmd
@@ -1,25 +1,24 @@
 @echo off
 
-setlocal enableextensions
-set EnableNuGetPackageRestore=true
+setlocal enableextensions enabledelayedexpansion
 
 pushd %~dp0
 
 call :ResolveNuGet nuget.exe || exit /b %ERRORLEVEL%
 
 if exist .nuget\packages.config (
-	echo Restoring packages from %~dp0.nuget\packages.config
-	"%NuGetExe%" restore .nuget\packages.config -PackagesDirectory packages -Verbosity quiet -NonInteractive || exit /b %ERRORLEVEL%
+	echo Restoring packages from !CD!\.nuget\packages.config
+	"%NuGetExe%" restore .nuget\packages.config -PackagesDirectory packages -NonInteractive -Verbosity quiet || exit /b !ERRORLEVEL!
 )
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /nologo /v:m /p:NuGetExecutable="%NuGetExe%" %*
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /nologo /v:m /p:NuGetExecutable="%NuGetExe%" /p:NuGetVerbosity=quiet %*
 
 popd
 goto :EOF
 
 :ResolveNuGet
 if exist %CD%\.nuget\%1 (
-	set NuGetExe=%CD%\.nuget\%1
+	set NuGetExe=!CD!\.nuget\%1
 	goto :EOF
 )
 set NuGetExe=%~$PATH:1
